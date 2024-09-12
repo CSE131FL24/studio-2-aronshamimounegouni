69fac20808d7989f689199731482267644a657e6
rgouni <rgouni@localhost>
2024-09-12 12:45:35 -0500

    not done

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,57 @@
+package studio2;
+
+import java.util.*;
+public class Ruin {
+
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Start amount: ");
+	int startAmount = scan.nextInt();
+	
+	System.out.println("Win probability: ");
+	double winChance = scan.nextDouble();
+	
+	System.out.println("Win limit: ");
+	int winLimit = scan.nextInt();
+	
+	System.out.println("Total simulations: ");
+	int totalSimulations = scan.nextInt();
+	
+for (int i = 1; i <= totalSimulations; i++) {
+	int numberOfDays = 0;
+	int simulations = 0;
+	while (startAmount > 0 && startAmount < winLimit)
+	{
+	
+		double chance = Math.random();
+		if (winChance >= chance)
+		{
+			startAmount++;
+			
+		
+		}
+		else 
+		{
+			startAmount--;
+			
+		}
+	numberOfDays++;
+	}
+	
+	if (startAmount == 0 )
+	{
+	System.out.println("Simulation " + (i) + ": " + numberOfDays + "LOSE");
+		
+	}
+	if (startAmount == winLimit)
+	{
+		System.out.println("Simulation " + (i) + ": " + numberOfDays + "WIN");
+	}
+	
+}
+	
+
+	}
+
+}
